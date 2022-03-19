21ab1105211765c2df56e5938b755fe5804579e0
WayneWayneHello <ahronwayne@gmail.com>
2022-03-19 18:39:49 -0400

    add slider to transpose X and Y

--- Fingerprint interface test.py
+++ Fingerprint interface test.py
@@ -29,8 +29,8 @@ def make_interface(window_name):
     
     file_path = easygui.fileopenbox()
 
-    cv2.namedWindow(window_name) #default, max. All have min of 0
-        
+    cv2.namedWindow(window_name, cv2.WINDOW_NORMAL) #default, max. All have min of 0
+    #cv2.resize(window_name,(200,400))
     cv2.createTrackbar('SizeCanv.',window_name,10,30, nothing)
     cv2.createTrackbar('MinSigma',window_name,25,100,nothing)
     cv2.createTrackbar('MaxSigma',window_name,75,100,nothing)
@@ -38,7 +38,8 @@ def make_interface(window_name):
     cv2.createTrackbar('HighPass',window_name,10,100,nothing)
     cv2.createTrackbar('Mean',window_name,0,1000,nothing)
     cv2.createTrackbar('Threshold',window_name,0,255,nothing)
-
+    cv2.createTrackbar('SwapXY',window_name,0,1,nothing)
+    
     return file_path
 
 def imfill2(image,radius,blank=0):
@@ -62,25 +63,29 @@ def get_values(window_name):
     HighPassFrequency = cv2.getTrackbarPos('HighPass',window_name)
     Mean = cv2.getTrackbarPos('Mean',window_name)
     Threshold = cv2.getTrackbarPos('Threshold',window_name)
-    return PPMMultiplier, STD_bounds_Lower, STD_bounds_Upper, ImFill, HighPassFrequency, Mean, Threshold
+    SwapXY = cv2.getTrackbarPos('SwapXY',window_name)
+    
+    return PPMMultiplier, STD_bounds_Lower, STD_bounds_Upper, ImFill, HighPassFrequency, Mean, Threshold,SwapXY
 
     
 #run the actual image processing dependent on interface
 
 def run_pipeline(source_dict, PPMMultiplier, STD_bounds_lower,
                  STD_bounds_upper, ImFill, HighPassFrequency,
-                 Mean, Threshold):
+                 Mean, Threshold,SwapXY):
+
 
     Xs = source_dict['Xs']
     Ys = source_dict['Ys']
-    Zs = source_dict['Zs']
 
+        
     XMin = source_dict['XMin']
-    YMin = source_dict['YMin']    
-    ZMin = source_dict['ZMin']
-
+    YMin = source_dict['YMin']
     XMax = source_dict['XMax']
     YMax = source_dict['YMax']
+        
+    Zs = source_dict['Zs']
+    ZMin = source_dict['ZMin']
     ZMax = source_dict['ZMax']
     
     PixelsPerMM = (len(Xs) / ((XMax-XMin) * (YMax-YMin)))**(1/2)
@@ -135,6 +140,9 @@ def run_pipeline(source_dict, PPMMultiplier, STD_bounds_lower,
 
     if Threshold:
         image = threshold(image,Threshold)
+
+    if SwapXY:
+        image = np.transpose(image)
         
     return(image)
     
@@ -144,8 +152,10 @@ def run_interface(window_name="Fingerprintsss"):
     file_path = make_interface(window_name)
     source_data = read_fingerprints(file_path)
 
-    Xs = source_data[:,0] #SWAPPED FOR THIS MACHINE
-    Ys = source_data[:,1]  #from CMM coordinates
+    
+    
+    Xs = source_data[:,0] 
+    Ys = source_data[:,1] 
     Zs = source_data[:,2]
 
     XMin,XMax = Xs.min(), Xs.max()
@@ -155,11 +165,11 @@ def run_interface(window_name="Fingerprintsss"):
     source_dict = {'Xs':Xs,'Ys':Ys,'Zs':Zs,'XMin':XMin,'XMax':XMax,'YMin':YMin,'YMax':YMax,'ZMin':ZMin,'ZMax':ZMax}
     
     
-    print('original ZMIN {} Max {}'.format(ZMin,ZMax))
+    print('original ZMIN {} ZMean{} ZMax {}'.format(ZMin,Zs.mean(),ZMax))
     
     
     while(1):
-        PPMMultiplier, STD_bounds_lower, STD_bounds_upper, ImFill, HighPassFrequency, Mean, Threshold = get_values(window_name)
+        PPMMultiplier, STD_bounds_lower, STD_bounds_upper, ImFill, HighPassFrequency, Mean, Threshold,SwapXY = get_values(window_name)
         PPMMultiplier = PPMMultiplier/10
         STD_bounds_lower = (STD_bounds_lower/10) - 5 #default: -2.5 to 2.5, total range of -5 to 5
         STD_bounds_upper = (STD_bounds_upper/10) - 5
@@ -171,7 +181,7 @@ def run_interface(window_name="Fingerprintsss"):
             newThreshold = abs(Threshold - 256) #0 still 0, but 1 is now white #lazy
             Threshold=newThreshold #for some reason, won't work without this             
         
-        image = run_pipeline(source_dict, PPMMultiplier, STD_bounds_lower, STD_bounds_upper, ImFill, HighPassFrequency, Mean, Threshold)
+        image = run_pipeline(source_dict, PPMMultiplier, STD_bounds_lower, STD_bounds_upper, ImFill, HighPassFrequency, Mean, Threshold,SwapXY)
         
         cv2.imshow('image',image.astype(np.uint8))
         #cv2.imshow(window_name,image.astype(np.uint8))
